9d092198727e1f204130d55b6d220fd5fd27fe84
ledainghia <92242188+ledainghia@users.noreply.github.com>
2023-09-17 12:46:52 +0700

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,54 @@ import './assets/css/iofrm-style.css';
 import './assets/css/iofrm-theme7.css';
 import './assets/css/bootstrap.min.css';
 import './assets/css/fontawesome-all.min.css';
+import { useEffect, useState } from 'react';
+import { HashLoader } from 'react-spinners';
+import './App.css'
+
 function App() {
 
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
 
   return (
-    <div className="form-body">
-      <div className="website-logo">
-        <a href="index.html">
-          <div className="logo">
-            <img className="logo-size" src={logo} alt="" />
+    <>
+      {
+        loading ?
+          <div style={{ textAlign: 'center', backgroundColor: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+            <HashLoader
+              color="rgba(255, 193, 166, 1)"
+              cssOverride={{}}
+              size={100}
+            />
           </div>
-        </a>
-      </div>
-      <div className="row">
-        <div className="img-holder">
-          <div className="bg"></div>
-          <div className="info-holder">
-            <img src={file} alt="" />
-          </div>
-        </div>
-        <LoginContent />
-      </div>
-    </div>
+          :
 
+          <div className="form-body">
+            <div className="website-logo">
+              <a href="index.html">
+                <div className="logo">
+                  <img className="logo-size" src={logo} alt="" />
+                </div>
+              </a>
+            </div>
+            <div className="row">
+              <div className="img-holder">
+                <div className="bg"></div>
+                <div className="info-holder">
+                  <img src={file} alt="" />
+                </div>
+              </div>
+              <LoginContent />
+            </div>
+          </div>
+      }
+    </>
   )
 }
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,81 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function Login() {
-    const [passwordVisible, setPasswordVisible] = useState(false); // State để theo dõi trạng thái của password (ẩn hoặc hiển thị)
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const userRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmitLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const user = userRef.current?.value;
+        const password = passwordRef.current?.value;
+        const data = {
+            user: user,
+            password: password,
+        };
+
+        const headers = {
+            'Server': 'openresty', // Thay thế YourAccessToken bằng token của bạn
+
+        };
+
+        const config = {
+            headers: headers,
+        };
+
+
+        await axios
+            .post('http://localhost:8083/api/login', data)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error.response);
+                toast.error('DUMA MAY LOI ROI CON', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+            })
+    }
 
     const togglePasswordVisibility = () => {
-        setPasswordVisible(!passwordVisible); // Khi bấm vào icon, chuyển đổi trạng thái
+        setPasswordVisible(!passwordVisible);
     };
 
     return (
-        <div>
-            <form>
-                <input className="form-control" type="text" name="username" placeholder="User name" required />
+        <>
+
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            {/* Same as */}
+            <ToastContainer />
+            <form
+                action="post"
+                onSubmit={handleSubmitLogin}
+                autoComplete="off"
+            >
+                <input className="form-control" ref={userRef} type="text" name="username" placeholder="User name" required />
                 <div className="password-container">
                     <input
                         className="form-control"
@@ -20,6 +83,7 @@ export default function Login() {
                         name="password"
                         id="password"
                         placeholder="Password"
+                        ref={passwordRef}
                         required
                     />
                     <FontAwesomeIcon
@@ -41,6 +105,6 @@ export default function Login() {
                     Google
                 </a>
             </div>
-        </div>
+        </>
     );
 }
