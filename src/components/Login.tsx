c41f44442b2a2dbeb0cf9c867b6fbcb0d2f4c900
ledainghia <92242188+ledainghia@users.noreply.github.com>
2023-09-18 23:34:07 +0700

    Update fix

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,6 +5,13 @@ import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useForgetStore } from '../store/store';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import jwt from 'jwt-decode'
+
+interface userGoogle {
+    name: string,
+    mail: string,
+}
 
 export default function Login() {
     const [passwordVisible, setPasswordVisible] = useState(false);
@@ -14,6 +21,8 @@ export default function Login() {
     const handleForget = () => {
         setForget(true); // Đặt isForgotten thành true
     };
+
+
 
     const handleSubmitLogin = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -107,9 +116,51 @@ export default function Login() {
             </form>
             <div className="other-links">
                 <span>Hoặc đăng nhập bằng</span>
-                <a href="#" style={{ color: "#ffa94d" }}>
+                {/* <a href="#" style={{ color: "#ffa94d" }}>
                     Google
-                </a>
+                </a> */}
+
+                <GoogleOAuthProvider clientId="673525613876-8si0uta7f5u7iq1po5rsj4lsur972vtj.apps.googleusercontent.com"><GoogleLogin
+                    onSuccess={async credentialResponse => {
+                        console.log(credentialResponse);
+                        const idToken = credentialResponse?.credential;
+                        if (idToken) {
+                            try {
+                                console.log(idToken);
+                                const user: userGoogle = jwt(idToken); // decode your token here
+                                console.log('Decoded Token: ', typeof (user));
+                                const name = user.name;
+                                toast.success(`Login success! Hi ${name}`, {
+                                    position: "top-right",
+                                    autoClose: 5000,
+                                    hideProgressBar: false,
+                                    closeOnClick: true,
+                                    pauseOnHover: true,
+                                    draggable: true,
+                                    progress: undefined,
+                                    theme: "light",
+                                });
+
+                            } catch (error) {
+                                console.error('Error decoding token:', error);
+                            }
+                        }
+                    }}
+                    onError={() => {
+                        console.log('Login Failed');
+                        toast.error('Login Failed', {
+                            position: "top-right",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                        });
+
+                    }}
+                /></GoogleOAuthProvider>
             </div>
         </>
     );
