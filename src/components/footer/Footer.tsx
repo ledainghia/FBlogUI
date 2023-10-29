7316c17aa05385968c6bf67f58cd5ef204697c95
haunguyendev <haunt150603@gmail.com>
2023-10-29 21:15:31 +0700

    create home and nav side

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 const Footer = () => {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">
+      <span>FuBlog Admin</span>
+      <span>© FuBlog Admin Dashboard</span>
+    </div>
+  );
 };
 
 export default Footer;
