12ef25836bc0989872955cb4893c73bf585fe6e7
haunguyendev <haunt150603@gmail.com>
2023-10-30 23:46:28 +0700

    add single page view

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,94 @@
+import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import DataTable from "../../components/dataTable/DataTable";
 import "./users.scss";
+import { userRows } from "../../data";
+import { useState } from "react";
+import Add from "../../components/Add/Add";
+
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: "img", headerName: "Avatar", width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "./noavatar.png"} alt="" />
+    }
+  },
+  // {field:"status",headerName:"Status",width:100,type:"boolean"},
+
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    type: "string",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    type: "string",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'email',
+    headerName: 'Email',
+    type: "string",
+    width: 200,
+    editable: true,
+  },
+  {
+    field: 'phone',
+    headerName: 'Phone',
+    type: "string",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'createdAt',
+    headerName: 'Create At',
+    type: "string",
+    width: 100,
+    editable: true,
+  },
+  {
+    field: 'verified',
+    headerName: 'Verified',
+    type: "boolean",
+    width: 100,
+    editable: true,
+  },
+
+  // {
+  //   field: 'fullName',
+  //   headerName: 'Full name',
+  //   description: 'This column has a value getter and is not sortable.',
+  //   sortable: false,
+  //   width: 160,
+  //   valueGetter: (params: GridValueGetterParams) =>
+  //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  // },
+];
+
+
 
 const Users = () => {
-  return <div className="users">Users</div>;
-};
+  const [open, setOpen] = useState(false);
+
+
+  return (
+
+
+
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button onClick={() => setOpen(true)}>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
+    </div>
+  )
+}
 
 export default Users;
