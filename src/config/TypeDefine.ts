cc729ae03449c1a6ca9986e499c357a269a54b3e
Lê Đại Nghĩa <nghialdse170198@fpt.edu.vn>
2023-11-14 21:55:10 +0700

    update

---
+++ src/config/TypeDefine.ts
@@ -0,0 +1,47 @@
+export interface blog {
+  typePost: string;
+  title: string;
+  content: string;
+  categoryName: string;
+  createdDate: string;
+  modifiedDate: Date;
+  approvedBy: number;
+  status: boolean;
+  isApproved: boolean;
+  picture: string;
+  views: number;
+  votes: [];
+  postComments: [];
+  tagList: tagList[];
+  postId: number;
+  user: userLogin;
+  blogPostCount: number;
+  commentCount: number;
+}
+
+export interface tagList {
+  tagId: number;
+  tagName: string;
+}
+
+export interface userLogin {
+  username: string;
+  id: number;
+  user: string;
+  email: string;
+  role: string;
+  roles: [];
+  fullName: string;
+  picture: string;
+  ranking: string;
+  point: number;
+  countViewOfBlog: number;
+  countVoteOfBlog: number;
+}
+
+export type categories = {
+  lable: string;
+  categoryId: number;
+  categoryName: string;
+  parentCategoryId: number | null;
+};
